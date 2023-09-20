4ddac45e4f4309c080d4ce6f3410d211bab6c314
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-09-20 15:18:18 +0200

    add bike class

---
+++ src/main/java/com/klodnicki/bike/rest/API/Bike/entity/Bike.java
@@ -0,0 +1,36 @@
+package com.klodnicki.bike.rest.API.Bike.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Bike {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String serialNumber;
+
+    public Bike(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public Bike() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+}

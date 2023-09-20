2d307eb78efc531917db451b99cbdd416bb6b27c
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-09-20 14:21:33 +0200

    Initial commit

---
+++ src/main/java/com/klodnicki/bike/rest/API/Bike/RentABikeRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.klodnicki.bike.rest.API.Bike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentABikeRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentABikeRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/klodnicki/bike/rest/API/Bike/RentABikeRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klodnicki.bike.rest.API.Bike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentABikeRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

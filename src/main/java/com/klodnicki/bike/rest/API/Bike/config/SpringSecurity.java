2c68127916def7a97f97b532f2db711318e575c9
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-09-21 18:22:57 +0200

    add spring security basic configuration

---
+++ src/main/java/com/klodnicki/bike/rest/API/Bike/config/SpringSecurity.java
@@ -0,0 +1,26 @@
+package com.klodnicki.bike.rest.API.Bike.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurity {
+
+@Bean
+public SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception {
+
+return httpSecurity.authorizeHttpRequests(request -> request
+        .requestMatchers("/api/admin").authenticated()
+        .requestMatchers("api/user").authenticated()
+        .anyRequest()
+        .permitAll())
+
+        .build();
+
+}
+
+}

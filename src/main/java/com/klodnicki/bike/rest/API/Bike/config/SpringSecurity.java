d910f1a381db4160aaacdb61959e59d4bb2fe7e1
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-09-22 10:52:40 +0200

    add roles in memory in spring config

--- src/main/java/com/klodnicki/bike/rest/API/Bike/config/SpringSecurity.java
+++ src/main/java/com/klodnicki/bike/rest/API/Bike/config/SpringSecurity.java
@@ -5,23 +5,43 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurity {
 
-@Bean
-public SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception {
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("user1")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin1")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
 
-return httpSecurity.authorizeHttpRequests(request -> request
-        .requestMatchers("/api/login/**").authenticated()
-        .anyRequest()
-        .permitAll())
-        .httpBasic(Customizer.withDefaults())
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
-        .build();
+        return httpSecurity.authorizeHttpRequests(request -> request
 
-}
+                        .requestMatchers("/api/v1").permitAll()
+                        .requestMatchers("/api/login/**").hasRole("ADMIN").anyRequest().authenticated())
+
+                        .httpBasic(Customizer.withDefaults())
+                        .build();
+    }
+
 
 }

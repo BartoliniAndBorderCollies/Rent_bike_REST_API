26dde28d0d6e57010b92b03e534d9e84be6c20d3
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-09-22 10:53:26 +0200

    add comments in UserController

--- src/main/java/com/klodnicki/bike/rest/API/Bike/controller/UserController.java
+++ src/main/java/com/klodnicki/bike/rest/API/Bike/controller/UserController.java
@@ -1,13 +1,23 @@
 package com.klodnicki.bike.rest.API.Bike.controller;
 
+import com.klodnicki.bike.rest.API.Bike.model.entity.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
-    @GetMapping("/login")
-    public String goToUser() {
+//    private final User user;
+//
+//    public UserController(User user) {
+//        this.user = user;
+//    }
+
+    @GetMapping("/api/login")
+    public String goToUser(Model model) {
+//        model.addAttribute("user", user);
+
         return "login";
     }
 

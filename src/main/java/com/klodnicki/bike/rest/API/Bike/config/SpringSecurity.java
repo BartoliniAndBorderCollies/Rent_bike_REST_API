ef6f67b889f73c1a51978b15fe3a9cce0670ac8f
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-09-23 10:05:36 +0200

    refactor of spring security config

--- src/main/java/com/klodnicki/bike/rest/API/Bike/config/SpringSecurity.java
+++ src/main/java/com/klodnicki/bike/rest/API/Bike/config/SpringSecurity.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -40,9 +41,10 @@ public class SpringSecurity {
         .requestMatchers(HttpMethod.GET, "/api/info").permitAll()
         .requestMatchers("/api/login/**").hasAnyRole("ADMIN", "USER")
         .anyRequest()
-        .authenticated())
+        .permitAll())
 
         .httpBasic(Customizer.withDefaults())
+        .csrf(AbstractHttpConfigurer::disable)
         .build();
     }
 

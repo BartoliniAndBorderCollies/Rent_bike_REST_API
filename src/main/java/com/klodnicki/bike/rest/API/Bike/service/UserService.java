9232d70b673d7575133ec74869fd8162244539b0
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-09-23 10:07:28 +0200

    add some CRUD operations (add user and find user) to service layer

--- src/main/java/com/klodnicki/bike/rest/API/Bike/service/UserService.java
+++ src/main/java/com/klodnicki/bike/rest/API/Bike/service/UserService.java
@@ -1,7 +1,33 @@
 package com.klodnicki.bike.rest.API.Bike.service;
 
+import com.klodnicki.bike.rest.API.Bike.exception.NotFoundInDatabaseException;
+import com.klodnicki.bike.rest.API.Bike.model.entity.User;
+import com.klodnicki.bike.rest.API.Bike.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User findUserById(Long id) throws NotFoundInDatabaseException {
+        Optional<User> optionalUser = userRepository.findById(id);
+
+        return optionalUser.orElseThrow(NotFoundInDatabaseException::new);
+
+    }
+
+    public Iterable<User> findAllUsers() {
+        return userRepository.findAll();
+    }
 }

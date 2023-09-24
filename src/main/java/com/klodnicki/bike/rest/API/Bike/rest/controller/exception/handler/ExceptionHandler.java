79dbcd124f058e6952a431686b054ac206b986b1
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-09-24 19:15:26 +0200

    move of exception handler to dedicated package

--- src/main/java/com/klodnicki/bike/rest/API/Bike/rest/controller/exception/handler/ExceptionHandler.java
+++ src/main/java/com/klodnicki/bike/rest/API/Bike/rest/controller/exception/handler/ExceptionHandler.java
@@ -1,5 +1,7 @@
-package com.klodnicki.bike.rest.API.Bike.exception;
+package com.klodnicki.bike.rest.API.Bike.rest.controller.exception.handler;
 
+import com.klodnicki.bike.rest.API.Bike.exception.NotFoundInDatabaseException;
+import com.klodnicki.bike.rest.API.Bike.exception.RestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestControllerAdvice;

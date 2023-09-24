06d54b95dabdee466f961c79925f24692c5917bc
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-09-24 19:17:21 +0200

    rename of the exception handler class

--- src/main/java/com/klodnicki/bike/rest/API/Bike/rest/controller/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/klodnicki/bike/rest/API/Bike/rest/controller/exception/handler/GlobalExceptionHandler.java
@@ -4,12 +4,13 @@ import com.klodnicki.bike.rest.API.Bike.exception.NotFoundInDatabaseException;
 import com.klodnicki.bike.rest.API.Bike.exception.RestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class ExceptionHandler {
+public class GlobalExceptionHandler {
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(NotFoundInDatabaseException.class)
+    @ExceptionHandler(NotFoundInDatabaseException.class)
     public ResponseEntity<RestException> handleNotFoundInDatabaseException(NotFoundInDatabaseException e) {
         return new ResponseEntity<>(new RestException("Nie ma takiego panie!", e.getMessage()), HttpStatus.NOT_FOUND);
     }

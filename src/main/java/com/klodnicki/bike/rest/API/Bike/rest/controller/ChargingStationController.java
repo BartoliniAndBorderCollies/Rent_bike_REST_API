27f3a9700d3c6ffa066188f4b4b35de3f9611840
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-09-24 09:08:34 +0200

    add method to add charging station in controller

--- src/main/java/com/klodnicki/bike/rest/API/Bike/rest/controller/ChargingStationController.java
+++ src/main/java/com/klodnicki/bike/rest/API/Bike/rest/controller/ChargingStationController.java
@@ -1,11 +1,23 @@
 package com.klodnicki.bike.rest.API.Bike.rest.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.klodnicki.bike.rest.API.Bike.model.entity.ChargingStation;
+import com.klodnicki.bike.rest.API.Bike.service.ChargingStationService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/station")
+@RequestMapping("/api/station")
 public class ChargingStationController {
+
+    private final ChargingStationService chargingStationService;
+
+    public ChargingStationController(ChargingStationService chargingStationService) {
+        this.chargingStationService = chargingStationService;
+    }
+
+    @PostMapping("/add")
+    public ChargingStation addChargingStation(@RequestBody ChargingStation chargingStation) {
+        return chargingStationService.addChargingStation(chargingStation);
+    }
 
 
 }

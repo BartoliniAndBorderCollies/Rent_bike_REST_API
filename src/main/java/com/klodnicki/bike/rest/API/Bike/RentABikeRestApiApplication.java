bcf6608fc69acf102e4e9e632937f4dede1a374b
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-09-26 19:51:59 +0200

    add comment with task description

--- src/main/java/com/klodnicki/bike/rest/API/Bike/RentABikeRestApiApplication.java
+++ src/main/java/com/klodnicki/bike/rest/API/Bike/RentABikeRestApiApplication.java
@@ -3,6 +3,15 @@ package com.klodnicki.bike.rest.API.Bike;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Napisz aplikację w Spring Boot, która będzie pozwalać na wypożyczanie rowerów miejskich za pomocą mobilnej aplikacji
+// na smartphone (Coś jak hulajnogi elektryczne bolta, na przykład).
+//		W mieście mamy stacje rowerowe, które służą do odstawiania tam rowerów i wypożyczania ich stamtąd.
+//		Każda stacja ma przyjazną dla użytkownika nazwę, którą łatwo zidentyfikować i rozpoznać oraz ograniczoną ilość
+//		miejsc dla rowerów. Nie można zostawić przy takiej stacji roweru, jeśli wszystkie miejsca są zajęte.
+//		Każdy rower ma również swoją nazwę lub numer, który go identyfikuje. Np. H0123 - tak, aby użytkownik również
+//		miał łatwość rozpoznania roweru, który chce wypożyczyć z aplikacji.
+//		Rowery obecnie dostępne to rowery elektryczne i tradycyjne.
+
 @SpringBootApplication
 public class RentABikeRestApiApplication {
 

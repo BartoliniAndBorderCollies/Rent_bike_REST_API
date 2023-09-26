e5e6700c688c3f4816522c16839dccb02f8ea212
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-09-26 08:59:51 +0200

    add findAllBikes() in controller

--- src/main/java/com/klodnicki/bike/rest/API/Bike/rest/controller/BikeController.java
+++ src/main/java/com/klodnicki/bike/rest/API/Bike/rest/controller/BikeController.java
@@ -2,10 +2,7 @@ package com.klodnicki.bike.rest.API.Bike.rest.controller;
 
 import com.klodnicki.bike.rest.API.Bike.model.entity.Bike;
 import com.klodnicki.bike.rest.API.Bike.service.BikeService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/bike")
@@ -17,10 +14,14 @@ public class BikeController {
         this.bikeService = bikeService;
     }
 
-
     @PostMapping("/add")
     public Bike addBike(@RequestBody Bike bike) {
         return bikeService.addBike(bike);
+    }
+
+    @GetMapping
+    public Iterable<Bike> findAllBikes() {
+        return bikeService.findAllBikes();
     }
 
 

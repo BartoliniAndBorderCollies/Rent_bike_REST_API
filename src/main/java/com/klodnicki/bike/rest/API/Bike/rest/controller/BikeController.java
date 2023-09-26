31fe767266f5fdb9f643e59fb9db55608dd1b922
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-09-26 08:54:59 +0200

    add BikeController with addBike()

--- src/main/java/com/klodnicki/bike/rest/API/Bike/rest/controller/BikeController.java
+++ src/main/java/com/klodnicki/bike/rest/API/Bike/rest/controller/BikeController.java
@@ -1,11 +1,29 @@
 package com.klodnicki.bike.rest.API.Bike.rest.controller;
 
+import com.klodnicki.bike.rest.API.Bike.model.entity.Bike;
+import com.klodnicki.bike.rest.API.Bike.service.BikeService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/bike")
+@RequestMapping("/api/bike")
 public class BikeController {
+
+    private final BikeService bikeService;
+
+    public BikeController(BikeService bikeService) {
+        this.bikeService = bikeService;
+    }
+
+
+    @PostMapping("/add")
+    public Bike addBike(@RequestBody Bike bike) {
+        return bikeService.addBike(bike);
+    }
+
+
 
 
 }

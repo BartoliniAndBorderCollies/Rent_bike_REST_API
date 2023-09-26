8d2b7e784b148f3eebfcdff458f616208d70123d
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-09-26 19:52:45 +0200

    add method to handle exception if validation requirement is not met

--- src/main/java/com/klodnicki/bike/rest/API/Bike/rest/controller/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/klodnicki/bike/rest/API/Bike/rest/controller/exception/handler/GlobalExceptionHandler.java
@@ -4,8 +4,10 @@ import com.klodnicki.bike.rest.API.Bike.exception.NotFoundInDatabaseException;
 import com.klodnicki.bike.rest.API.Bike.exception.RestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -13,5 +15,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NotFoundInDatabaseException.class)
     public ResponseEntity<RestException> handleNotFoundInDatabaseException(NotFoundInDatabaseException e) {
         return new ResponseEntity<>(new RestException("Nie ma takiego panie!", e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<RestException> handleValidationRequirementNotMet(MethodArgumentNotValidException e) {
+        return new ResponseEntity<>(new RestException("Validation error!", e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 }

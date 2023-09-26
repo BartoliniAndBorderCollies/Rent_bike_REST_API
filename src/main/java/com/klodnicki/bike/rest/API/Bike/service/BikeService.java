dac7e77ea0282db5f6eb47213a23d77cc6c030b0
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-09-26 08:54:38 +0200

    add BikeService with addBike()

---
+++ src/main/java/com/klodnicki/bike/rest/API/Bike/service/BikeService.java
@@ -0,0 +1,21 @@
+package com.klodnicki.bike.rest.API.Bike.service;
+
+import com.klodnicki.bike.rest.API.Bike.model.entity.Bike;
+import com.klodnicki.bike.rest.API.Bike.repository.BikeRepository;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class BikeService {
+
+    private final BikeRepository bikeRepository;
+
+
+    public BikeService(BikeRepository bikeRepository) {
+        this.bikeRepository = bikeRepository;
+    }
+
+    public Bike addBike(Bike bike) {
+        return bikeRepository.save(bike);
+    }
+}

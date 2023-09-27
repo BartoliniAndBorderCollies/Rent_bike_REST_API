40f864937dc66a14f66e00a56c48513edaa2d7e3
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-09-27 16:33:47 +0200

    add field and relation OneToMany

--- src/main/java/com/klodnicki/bike/rest/API/Bike/model/entity/ChargingStation.java
+++ src/main/java/com/klodnicki/bike/rest/API/Bike/model/entity/ChargingStation.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
+
+import java.util.List;
+
 @Entity
 public class ChargingStation {
     @Id
@@ -17,14 +20,17 @@ public class ChargingStation {
     @NotNull(message = "Must have a value.")
     @PositiveOrZero (message = "Must be equal or bigger than zero!")
     private int freeSlots;
+    @OneToMany (mappedBy = "chargingStation")
+    private List<Bike> bikeList;
 
     public ChargingStation() {
     }
 
-    public ChargingStation(String name, int freeSlots) {
-
+    public ChargingStation(Long id, String name, int freeSlots, List<Bike> bikeList) {
+        this.id = id;
         this.name = name;
         this.freeSlots = freeSlots;
+        this.bikeList = bikeList;
     }
 
     public Long getId() {

5ca4fde8cb6b1f8240976a3d2f0a0a160f3822d9
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-09-29 15:59:02 +0200

    add cascade in User class

--- src/main/java/com/klodnicki/bike/rest/API/Bike/model/entity/User.java
+++ src/main/java/com/klodnicki/bike/rest/API/Bike/model/entity/User.java
@@ -37,7 +37,7 @@ public class User {
     @NotBlank(message = "Must have a value.")
     private String role;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "bike_id")
     private Bike bike;
 

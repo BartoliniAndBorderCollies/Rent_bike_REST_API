83a8408b47bc6592aabb6c605f2c09a69b899243
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-01 09:33:31 +0200

    solve the problem with setting user to bike

--- src/main/java/com/klodnicki/bike/rest/API/Bike/service/BikeService.java
+++ src/main/java/com/klodnicki/bike/rest/API/Bike/service/BikeService.java
@@ -3,7 +3,6 @@ package com.klodnicki.bike.rest.API.Bike.service;
 import com.klodnicki.bike.rest.API.Bike.exception.NotFoundInDatabaseException;
 import com.klodnicki.bike.rest.API.Bike.model.dto.BikeForNormalUserDTO;
 import com.klodnicki.bike.rest.API.Bike.model.entity.Bike;
-import com.klodnicki.bike.rest.API.Bike.model.entity.ChargingStation;
 import com.klodnicki.bike.rest.API.Bike.repository.BikeRepository;
 import org.springframework.stereotype.Service;
 
@@ -42,6 +41,17 @@ public class BikeService {
         bike.setSerialNumber(bikeToUpdate.getSerialNumber());
         bike.setRented(bikeToUpdate.isRented());
         bike.setBikeType(bikeToUpdate.getBikeType());
+        bike.setUser(bikeToUpdate.getUser()); //user doesn't work (400 bad request) -> it works, I forgot to put object
+        //into object in JSON (user is an object and I passed String in postman)
+        //if I updated a bike with user it didn't show up when I send GET request on a bike. the response was that
+        //bike has a field user with null value. The reason for this was that @OneToOne relationship between User and
+        // Bike, with User being the owning side of the relationship. This means that if I set the User on the Bike,
+        // but don’t set the Bike on the User, then JPA won’t know about the relationship and won’t update the
+        // database accordingly.
+        // To fix this, I should also set the Bike on the User when I'm setting the User on the Bike:
+        bikeToUpdate.getUser().setBike(bike);
+
+
         if(bikeToUpdate.isRented()) {
             if(bike.getChargingStation() != null) {
                 bike.getChargingStation().getBikeList().remove(bikeToUpdate);
@@ -53,9 +63,6 @@ public class BikeService {
             bike.setChargingStation(bikeToUpdate.getChargingStation());//doesn't save this information in db, now it does
             //I must have erased @JsonIgnore and use @JsonIdentityInfo instead
         }
-        bike.setUser(bikeToUpdate.getUser()); //user doesn't work (400 bad request) -> it works, I forgot to put object
-        //into object in JSON (user is an object and I passed String in postman)
-
         return bikeRepository.save(bike);
     }
 
